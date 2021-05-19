52e94866105eb97945bc4ae952cc6e21aabfb207
Issam Mohamed Hammadi <72212886+issamhammadi19@users.noreply.github.com>
2021-05-19 18:56:42 +0100

    Add files via upload

---
+++ charToAsciiCode.c
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <stdlib.h>
+void toAscii(char letter);
+int main(){
+	printf("------------Program to convert a char to ASCII code by Issam------------\n");
+	char letter;
+	printf("Enter a letter: ");
+	scanf("%c", &letter);
+	toAscii(letter);
+	int x;
+		printf("Do you want to try again?(1 = yes ,0 = no): ");
+		scanf("%d", &x);
+	while (x==1) {
+
+		printf("Enter a letter: ");
+		scanf(" %c", &letter);
+		toAscii(letter);
+		printf("Do you want to try again?(1 = yes ,0 = no): ");
+		scanf("%d", &x);
+	}
+		printf("Thanks\n");
+		printf("All copyright reserved");
+	return 0;
+}
+void toAscii(char letter) {
+	switch (letter) {
+	case '0':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case '1':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case '2':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case '3':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case '4':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case '5':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case '6':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case '7':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case '8':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case '9':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'A':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'B':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'C':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'D':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'E':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'F':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'G':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'H':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'I':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'J':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'K':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'L':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'M':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'N':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'O':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'P':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'Q':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'R':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'S':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'T':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'U':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'V':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'W':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'X':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'Y':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'Z':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'a':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'b':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'c':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'd':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'e':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'f':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'g':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'h':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'i':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'j':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'k':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'l':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'm':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'n':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'o':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'p':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'q':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'r':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 's':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 't':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'u':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'v':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'w':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'x':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'y':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	case 'z':
+		printf("ASCII code of %c is %d\n", letter, letter);
+		break;
+	default:
+		printf("Invalid input\n");
+	}
+}
+
+
